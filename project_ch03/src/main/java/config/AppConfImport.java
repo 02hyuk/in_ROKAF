246636a8c35b7ec43fb701ae7e699497b92113f8
chanhyuk <chyun0222@gmail.com>
2023-05-22 10:29:36 +0000

    study in 23.05.22_v3(two config class, using @Autowired, @Import)

--- project_ch03/src/main/java/config/AppConfImport.java
+++ project_ch03/src/main/java/config/AppConfImport.java
@@ -2,13 +2,15 @@ package config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import spring.MemberDao;
 import spring.MemberPrinter;
 
 // main 메소드에서 쓸 스프링 컨테이너의 설정 클래스 1
 @Configuration
-public class AppConf1 {
+@Import(AppConf2.class) // 컨테이너 생성시 AppConf2의 설정까지 포함하겠다는 뜻
+public class AppConfImport {
     @Bean
     public MemberDao memberDao() {
         return new MemberDao();
